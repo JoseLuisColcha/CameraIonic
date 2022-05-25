885869f9380c8569a37a630b963a4c0c8a27ae47
JoseLuisColcha <jose.colcha@epn.edu.ec>
2022-05-25 00:45:16 -0500

    Gallery implemented

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,24 @@
+/* eslint-disable @angular-eslint/use-lifecycle-interface */
+
 import { Component } from '@angular/core';
-import { PhotoService } from '../services/photo.service';
+import { PhotoService, UserPhoto } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  public photos: UserPhoto[] = [];
 
-  constructor(public photoService: PhotoService) {}
-  addPhotoGallery(){
+  constructor(public photoService: PhotoService) {
+
+  }
+
+addPhotoGallery(){
     this.photoService.addNewToGallery();
+  }
+  async ngOnInit() {
+    await this.photoService.loadSaved();
   }
 }
